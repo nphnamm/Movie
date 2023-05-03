98a2786eb8d893630e8fc44cb4287f98db03122d
Nphnam <nphnamm@gmail.com>
2023-05-03 13:06:20 +0700

    new code
    new code may 3th

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ $(document).ready(() =>{
         $('#hamburger-menu').toggleClass('active');
         $('#nav-menu').toggleClass('active');
     })
+    // setting owl carousel
+    let navText = ["<i class='bx bx-chevron-left'></i>",
+    "<i class='bx bx-chevron-right'></i>"]
+    $('#hero-carousel').owlCarousel({
+        items:1,
+        dots:false,
+        loop:true,
+        nav:true,
+        navText: navText,
+        autoplayHoverPause:true
+
+        })
+
+
 })
